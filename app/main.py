df898885878d461ce309efbda823a787911897af
Antonov <krasimirov_a@abv.bg>
2024-08-22 11:25:51 +0200

    run tests

--- app/main.py
+++ app/main.py
@@ -11,12 +11,11 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
 
+
+for x in conn:
     with conn:
        conn.recv(1024)
        conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
-
-c = input.int()
-print(type(c))
